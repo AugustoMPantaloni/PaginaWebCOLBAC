b11d9ebdae76b14fd0ca053ca4e9e9843623570d
Franco Plomer <franco_plomer@itrsa.com.ar>
2025-03-30 17:34:49 -0300

    hot fix stypes ourJobs module

---
+++ src/components/nuestroTrabajo/columnOurJob/columnOurJob.wrapper.jsx
@@ -0,0 +1,36 @@
+
+//Css
+import "./OurJob.css"
+
+
+const ColumnOurJob = ({ tittle, content, imgSRC, idx }) =>{
+    return(
+        <div 
+            className={ 
+                'contenedorOurJob' + ( 
+                    idx % 2 === 0 ? 
+                    ' zebraEffect' : 
+                    ''
+                ) 
+            }
+        >
+            {
+                imgSRC ?
+                <div> 
+                    <img  className="iconoOurJob" src={imgSRC}/> 
+                </div> :
+                ''
+            }
+            {
+                tittle ?
+                <div className="contenedorTextoOurJob"> 
+                    <h2>{ tittle }</h2>
+                    <p>{ content }</p>
+                </div> :
+                ''
+            }
+        </div>
+    )
+}
+
+export default ColumnOurJob
---
+++ src/components/nuestroTrabajo/constants/ourJobSecondList.jsx
@@ -0,0 +1,41 @@
+import iconoDiseño from "../../../assets/iconosNuestroTrabajo/iconoDiseño.png"
+import iconoPruebas from "../../../assets/iconosNuestroTrabajo/iconoPruebas.png"
+import iconoMantenimiento from "../../../assets/iconosNuestroTrabajo/iconoMantenimiento.png"
+
+export const secondList = [
+
+    {
+        tittle: 'Planificación y análisis',
+        content: `Todo empieza con una charla. Queremos conocer tu idea,
+        entender qué  necesitas y para quién será el producto.
+        Analizamos los requisitos y definimos los objetivos y funcionalidades clave.  
+        En esta etapa, trazamos el plan que guiará todo el desarrollo.`
+    },
+
+    {
+        imgSRC: iconoDiseño
+    },
+
+    {
+        tittle: 'Desarrollo',
+        content: `Redactando el código. Nuestros desarrolladores crean cada funcionalidad planificada, 
+        construyendo el backend, el frontend y asegurando que todo fluya en perfecta armonía. 
+        Trabajamos en fases para garantizar estabilidad y cumplir con lo acordado.`
+    },
+
+    {
+        imgSRC: iconoPruebas
+    },
+
+    {
+        tittle: 'Despliegue',
+        content: `Cuando todo está listo, es momento de ponerlo en marcha.
+        Subimos el software a servidores, lo distribuimos 
+        y nos aseguramos de que esté accesible
+        y funcionando sin problemas.`
+    },
+
+    {
+        imgSRC: iconoMantenimiento
+    },
+];
